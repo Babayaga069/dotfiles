3491ddbf3e53412372474cfbf4567e11e89b3e73
admin <gittea@mail.com>
2026-03-05 12:06:41 +0100

    change install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,19 @@
 set -e
 DOTFILES="$HOME/.dotfiles"
 
-echo "→ Symlinks setzen..."
+echo "-> Pakete installieren..."
+sudo apt install -y git zsh neovim tmux
 
-ln -sf "$DOTFILES/zsh/.zshrc"         "$HOME/.zshrc"
-ln -sf "$DOTFILES/tmux/.tmux.conf"    "$HOME/.tmux.conf"
-ln -sf "$DOTFILES/nvim"               "$HOME/.config/nvim"
-ln -sf "$DOTFILES/alacritty"          "$HOME/.config/alacritty"
+echo "-> Lazygit installieren... "
+curl -Lo /tmp/lazygit.tar.gz https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_Linux_x86_64.tar.gz
+tar -xzf /tmp/lazygit.tar.gz -C /tmp
+sudo mv /tmp/lazygit /usr/local/bin/
+
+echo "→ Symlinks setzen..."
+ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES/nvim" "$HOME/.config/nvim"
+ln -sf "$DOTFILES/alacritty" "$HOME/.config/alacritty"
 
 echo "→ Shell auf zsh setzen..."
 chsh -s $(which zsh)
